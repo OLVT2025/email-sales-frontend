baeb54d17c3796633108c7bf6fbb10af7ee016bf
Sunke Venkateswarlu <sunkevenkateswarlu@Sunkes-MacBook-Air.local>
2025-03-11 13:42:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import EmailDashboard from './EmailDashboard';
-import FileExplorer from './FileExplorer';
 
 function App() {
   return (
     <div className="App">
       <EmailDashboard/>
-      {/* <FileExplorer/> */}
     </div>
   );
 }
